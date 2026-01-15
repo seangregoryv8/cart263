ce2759ce9d5455b728676ad2371780f1468e9c73
Sean Gregory <seangregoryv8@gmail.com>
2026-01-14 19:01:39 -0500

    Added the random menu

---
+++ tasks/1/js/question1.js
@@ -0,0 +1,4 @@
+function question1()
+{
+    console.log("HELLO WORLD")
+}
--- tasks/1/js/script.js
+++ tasks/1/js/script.js
@@ -19,5 +19,30 @@ function setup()
 
 function draw()
 {
-    
+    push();
+    fill(255);
+    textSize(32);
+    text("Funky Town by Sean Gregory and Leah Song", 100, 100)
+    textSize(24);
+    text("Choose a question that I answered", 100, 200)
+
+    translate(100, 250);
+    textSize(18);
+    for (let i = 1; i <= 7; i++)
+    {
+        text("Question " + i, 0, i * 60);
+    }
+    pop();
+
+    if (questionAsked == 1)
+    {
+        question1();
+    }
+}
+
+let questionAsked = 0;
+
+document.onkeydown = function(e)
+{
+    if (e.key === "1") questionAsked = 1;
 }
