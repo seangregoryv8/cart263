51993069500ade25ccac2b6f9fab18a96ffd31d4
Sean Gregory <seangregoryv8@gmail.com>
2026-01-14 19:15:01 -0500

    Added question 1

--- tasks/1/js/question1.js
+++ tasks/1/js/question1.js
@@ -1,4 +1,9 @@
 function question1()
 {
-    console.log("HELLO WORLD")
+    fill(255, 0, 0)
+    ellipse(30, 30, 50, 50);
+    fill(0, 255, 0)
+    ellipse(80, 80, 70, 70);
+    fill(0, 0, 255)
+    ellipse(145, 145, 90, 90);
 }
--- tasks/1/js/script.js
+++ tasks/1/js/script.js
@@ -20,24 +20,49 @@ function setup()
 function draw()
 {
     push();
-    fill(255);
-    textSize(32);
-    text("Funky Town by Sean Gregory and Leah Song", 100, 100)
-    textSize(24);
-    text("Choose a question that I answered", 100, 200)
 
-    translate(100, 250);
-    textSize(18);
-    for (let i = 1; i <= 7; i++)
+    switch (questionAsked)
     {
-        text("Question " + i, 0, i * 60);
+        case 0:
+            fill(255);
+            textSize(32);
+            //text("Funky Town by Sean Gregory and Leah Song", 100, 100)
+            textSize(24);
+            //text("Choose a question that I answered", 100, 200)
+        
+            translate(100, 250);
+            textSize(18);
+            for (let i = 1; i <= 7; i++)
+            {
+                //text("Question " + i, 0, i * 60);
+            }
+            break;
+        case 1:
+            question1();
+            break;
+        case 2:
+            question2();
+            break;
+        case 3:
+            question3();
+            break;
+        case 4:
+            question4();
+            break;
+        case 5:
+            question5();
+            break;
+        case 6:
+            question6();
+            break;
+        case 7:
+            question7();
+            break;
+
+
     }
+
     pop();
-
-    if (questionAsked == 1)
-    {
-        question1();
-    }
 }
 
 let questionAsked = 0;
@@ -45,4 +70,10 @@ let questionAsked = 0;
 document.onkeydown = function(e)
 {
     if (e.key === "1") questionAsked = 1;
+    if (e.key === "2") questionAsked = 2;
+    if (e.key === "3") questionAsked = 3;
+    if (e.key === "4") questionAsked = 4;
+    if (e.key === "5") questionAsked = 5;
+    if (e.key === "6") questionAsked = 6;
+    if (e.key === "7") questionAsked = 7;
 }
