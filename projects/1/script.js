c8e3c0ae51ad3ed22fc866c8d3f383aacae11ffc
Sean Gregory <chiefsgreg2622@gmail.com>
2026-03-05 20:57:54 -0500

    Managed a basic cube creation system

--- projects/1/script.js
+++ projects/1/script.js
@@ -7,30 +7,108 @@ function addToConsole()
     document.getElementById("consoleInput").value = "";
     if (newText == "") return;
 
-    document.getElementById("consoleOutput").innerHTML += "<br>" + newText;
+    document.getElementById("consoleOutput").innerHTML += "> " + newText + "<br>";
 
     determineCommand(newText);
 }
 
+function inputToConsole(text)
+{
+    document.getElementById("consoleOutput").innerHTML += text + "<br>";
+}
 function determineCommand(newText)
 {
-    switch (newText)
+    newText = newText.toLowerCase();
+    switch (newText.split(" ")[0])
     {
         case "help":
-            document.getElementById("consoleOutput").innerHTML += "<br>Available commands: help, clear";
+            inputToConsole("Available commands: help, clear<br>");
             break;
         case "clear":
             document.getElementById("consoleOutput").innerHTML = "";
             break;
+        case "cube":
+            createCube(newText);
+            break;
+        case "reset":
+            location.reload();
+            break;
+        case "erase":
+            let cubes = document.querySelectorAll("div.cube");
+            cubes.forEach(cube => {
+                cube.remove();
+            });
+            inputToConsole("All cubes have been erased.");
+            break;
         default:
-            document.getElementById("consoleOutput").innerHTML += "<br>Unknown command. Type 'help' for available commands.";
+            inputToConsole("Unknown command. Type 'help' for available commands.<br>");
     }
 }
 
+function createCube(input)
+{
+    let inputs = input.split(" ");
+
+    console.log(inputs.length);
+
+    if (inputs[1] == "help")
+    {
+        inputToConsole("There are 5 uses for this command:");
+        inputToConsole("1. cube - Creates a 100x100 cube at x: 100, y: 100");
+        inputToConsole("2. cube [size] - creates a cube with the specified size at x: 100, y: 100");
+        inputToConsole("3. cube [x] [y] - creates a 100x100 cube at the specified position.");
+        inputToConsole("4. cube [size] [x] [y] - creates a cube with the specified size at the specified position.");
+        inputToConsole("5. cube [width] [height] [x] [y] - creates a cube with the specified width and height at the specified position.");
+        return;
+    }
+    let width = 100;
+    let height = 100;
+    let x = 100;
+    let y = 100;
+    switch (inputs.length)
+    {
+        case 2:
+            // Default position, custom size
+            width = parseInt(inputs[1]);
+            height = parseInt(inputs[1]);
+            break;
+        case 3:
+            // Default size, custom position
+            x = parseInt(inputs[1]);
+            y = parseInt(inputs[2]);
+            break;
+        case 4:
+            // Custom size, custom position
+            width = parseInt(inputs[1]);
+            height = parseInt(inputs[1]);
+            x = parseInt(inputs[2]);
+            y = parseInt(inputs[3]);
+            break;
+        case 5:
+            // Custom rectangular size, custom position
+            width = parseInt(inputs[1]);
+            height = parseInt(inputs[2]);
+            x = parseInt(inputs[3]);
+            y = parseInt(inputs[3]);
+            break;
+    }
+
+    let cube = document.createElement("div");
+    cube.classList.add("cube");
+    cube.style.width = width + "px";
+    cube.style.height = height + "px";
+    cube.style.backgroundColor = "red";
+    cube.style.position = "absolute";
+    cube.style.left = x + "px";
+    cube.style.top = y + "px";
+    // random colour for the cube
+    cube.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    document.body.appendChild(cube);
+    inputToConsole("You have created a cube!");
+}
 
 
 addEventListener("keydown", function(event) {
-    if (event.key == "Enter") {
+    if (event.key == "Enter")
         addToConsole();
-    }
 });
